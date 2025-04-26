46efa20296bc6837ed3afc0c3ea2c1c853453bfa
Darien Parris <dparri03@login-prod-01.pax.tufts.edu>
2025-04-26 16:32:30 -0400

    remove shell script and reformat python file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from huggingface_hub import hf_hub_download,snapshot_download
+from huggingface_hub import hf_hub_download, snapshot_download
 from tensorflow.keras.models import load_model
 import numpy as np
 import torch
@@ -8,12 +8,14 @@ import sys
 import os
 
 # Add the 'stylegan2-pytorch' subdirectory to the sys.path
-sys.path.append(os.path.join(os.path.dirname(__file__), 'stylegan2-pytorch'))
+sys.path.append(os.path.join(os.path.dirname(__file__), "stylegan2-pytorch"))
 
 from model import Generator
 
 STYLE_GAN_REPO = "ZeqiangLai/StyleGAN2"
-STYLE_GAN_VERSION = "bb4e00dc8ac0afbb9e0a5b6f72754c679673ee2e" # Ensures we always get the same model
+STYLE_GAN_VERSION = (
+    "bb4e00dc8ac0afbb9e0a5b6f72754c679673ee2e"  # Ensures we always get the same model
+)
 STYLE_GAN_FILE = "stylegan2-car-config-f.pt"
 
 REPO = "dima806/deepfake_vs_real_image_detection"
@@ -28,7 +30,7 @@ model_path = model_path + "/" + STYLE_GAN_FILE
 
 G = Generator(size=1024, style_dim=512, n_mlp=8).cuda()
 
-with open(model_path, 'rb') as f:
+with open(model_path, "rb") as f:
     hf_weights = torch.load(f, map_location=device)["g_ema"]
 
 G.load_state_dict(hf_weights["g_ema"])
@@ -40,12 +42,13 @@ z = torch.randn(1, G.z_dim).cuda()
 img = G(z, None)  # (N, C, H, W) in [-1, 1] range
 
 # Convert to image format (0-255 uint8)
-img = (img.clamp(-1, 1) + 1) * (255/2)
+img = (img.clamp(-1, 1) + 1) * (255 / 2)
 img = img.permute(0, 2, 3, 1)  # (N, H, W, C)
-img = img[0].cpu().numpy().astype('uint8')
+img = img[0].cpu().numpy().astype("uint8")
 
 # Save it
 from PIL import Image
+
 Image.fromarray(img).save("sample_output.png")
 """
 if __name__ == '__main__':
--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-module load anaconda/2021.05
-conda create -n env310 python=3.10
-
-/cluster/tufts/hpc/tools/anaconda/202105/bin/python -m pip install --upgrade pip
-pip install -r requirements.txt
-
-python main.py

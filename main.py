5eef51d347418a7aa262e967b637e37e76e3fcae
Darien Parris <dparri03@login-prod-01.pax.tufts.edu>
2025-04-26 16:24:43 -0400

    runs, now just need to get CUDA to cooperate

--- main.py
+++ main.py
@@ -2,52 +2,56 @@ from huggingface_hub import hf_hub_download,snapshot_download
 from tensorflow.keras.models import load_model
 import numpy as np
 import torch
+import pickle
 import PIL.Image
 import sys
+import os
+
+# Add the 'stylegan2-pytorch' subdirectory to the sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'stylegan2-pytorch'))
+
+from model import Generator
 
 STYLE_GAN_REPO = "ZeqiangLai/StyleGAN2"
 STYLE_GAN_VERSION = "bb4e00dc8ac0afbb9e0a5b6f72754c679673ee2e" # Ensures we always get the same model
 STYLE_GAN_FILE = "stylegan2-car-config-f.pt"
 
+REPO = "dima806/deepfake_vs_real_image_detection"
+
 # Download model repo to a local directory
-model_path = snapshot_download(repo_id=STYLE_GAN_REPO, )
-print(f"Model downloaded to: {model_path}")
+model_path = snapshot_download(repo_id=STYLE_GAN_REPO)
+print("Model downloaded to:", model_path)
 
 # Load generator
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model_path = f"{model_path}/{STYLE_GAN_FILE}"
+model_path = model_path + "/" + STYLE_GAN_FILE
 
-sys.path.append(model_path)
+G = Generator(size=1024, style_dim=512, n_mlp=8).cuda()
 
-from generator import StyleGAN2Generator
+with open(model_path, 'rb') as f:
+    hf_weights = torch.load(f, map_location=device)["g_ema"]
 
-G = StyleGAN2Generator(size=1024, style_dim=512, n_mlp=8).to(device)
+G.load_state_dict(hf_weights["g_ema"])
 
-ckpt = torch.load(model_path, map_location=device)
-G.load_state_dict(ckpt["g_ema"], strict=False)
-G.eval()
-"""
-with open(f"{local_path}/{STYLE_GAN_FILE}", 'rb') as f:
-    G = pickle.load(f)['G_ema'].cuda()
-"""
-
-# Generate latent vector
-z = torch.from_numpy(np.random.randn(1, G.z_dim)).cuda()
+# Generate random latent vector
+z = torch.randn(1, G.z_dim).cuda()
 
 # Generate image
-img = G(z, None)[0]
-img = (img.clamp(-1, 1) + 1) * (255 / 2)
-img = img.permute(1, 2, 0).to('cpu', torch.uint8).numpy()
-PIL.Image.fromarray(img).save("fake_face.png")
+img = G(z, None)  # (N, C, H, W) in [-1, 1] range
 
+# Convert to image format (0-255 uint8)
+img = (img.clamp(-1, 1) + 1) * (255/2)
+img = img.permute(0, 2, 3, 1)  # (N, H, W, C)
+img = img[0].cpu().numpy().astype('uint8')
 
-
-
-
+# Save it
+from PIL import Image
+Image.fromarray(img).save("sample_output.png")
+"""
 if __name__ == '__main__':
     # Download ONNX face swap model from Hugging Face
     model_path = hf_hub_download(repo_id=STYLE_GAN_REPO, filename=STYLE_GAN_FILE)
-    print(f"Model downloaded to: {model_path}")
+    print("Model downloaded to: ", model_path)
 
     # Load the model
     model = load_model(model_path)
@@ -61,3 +65,4 @@ if __name__ == '__main__':
     img = np.clip(generated, 0, 255).astype(np.uint8)
     PIL.Image.fromarray(img).save("stylegan2_output.png")
 
+"""

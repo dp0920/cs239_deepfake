be060c14b6ac32f453134a679d5f493192055075
Darien Parris <darien.parris@tufts.edu>
2025-04-30 19:06:58 -0400

    new runs and updated batch runner script

--- sbatch_runner.sh
+++ sbatch_runner.sh
@@ -2,10 +2,10 @@
 
 # This script starts sbatch runs for each supported GPU
 
-GPUs=("p100" "v100" "a100")
+#GPUs=("p100" "a100")
+GPUs=("p100" "a100")
 
-for GPU in "${GPUs}[@]"; do
-sbatch --job-name=stylegan_$GPU_experiment --output=stylegan_$GPU_experiment_%j.log --error=stylegan_$GPU_experiment_%j.err --time=0-02:00 --mem=4G --partition=preempt --gres=gpu:$GPU:1 experiments/experiment.sh $(pwd)/venv310
-
+for GPU in "${GPUs[@]}"; do
+	sbatch --job-name=stylegan_$GPU_experiment --output=stylegan_$GPU_experiment_%j.log --error=stylegan_$GPU_experiment_%j.err --time=0-04:00 --mem=4G --partition=gpu --gres=gpu:$GPU:1 experiments/experiment.sh $(pwd)/venv310
 done
 
